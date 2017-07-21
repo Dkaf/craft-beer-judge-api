6d37e5b19b6f90992678d8995e101d172420ff9a
daniel kafer <daniel.j.kafer@gmail.com>
2017-07-21 16:36:26 -0500

    added mongoose connection to local db

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
+import routes from './routes';
+
+mongoose.connect('mongodb://localhost:27017/craft_beer_judge', () => {
+	console.log('connected to mongodb...');
+});
 
 const app = express();
 
+//Middleware
 app.use(bodyParser.json());
+
+app.use('/api', routes);
 
 export default app;
