1852656749eed30658767c7248e9b6b72a81cfb7
daniel kafer <daniel.j.kafer@gmail.com>
2017-07-21 16:36:48 -0500

    Created User model

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+mongoose.Promise = global.Promise;
+
+const userSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+		minlength: [4, 'Username must be at least 4 characters']
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: [8, 'Password must be at least 8 characters']
+	}
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
