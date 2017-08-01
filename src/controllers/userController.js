db7a92d15610812e349133e39f7f00fa0b6a0670
daniel kafer <daniel.j.kafer@gmail.com>
2017-08-01 16:16:50 -0500

    added login and get user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -27,7 +27,24 @@ userController.postUser = (req, res) => {
 userController.getUser = (req, res) => {
 	const { username } = req.body;
 
+	db.User.find({username: username})
+		.then((user) => {
+			res.status(200).json({
+				success: true,
+				data: user
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({
+				message: err
+			});
+		});
 
+};
+
+//Login
+userController.getLogin = (req, res) => {
+	res.status(200).json({success: true});
 };
 
 export default userController;
