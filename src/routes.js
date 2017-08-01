2a1408142f92ca40908abd6875a754cc76a82d39
daniel kafer <daniel.j.kafer@gmail.com>
2017-08-01 16:15:02 -0500

    added beer and beer fridge routes

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,20 @@ const routes = express();
 
 //Controller Imports
 import userController from './controllers/userController';
+import beerController from './controllers/beerController';
+import beerFridgeController from './controllers/beerFridgeController';
 
 //User Routes
 routes.post('/signup', userController.postUser);
+
+//Beer Routes
+routes.get('/beers/:name/:p', beerController.getBeers);
+routes.get('/categories', beerController.getCategories);
+routes.get('/categories/:categoryId', beerController.getSingleCategory);
+
+//Beer Fridge Routes
+routes.get('/beerFridge', beerFridgeController.getUserFridge);
+routes.get('/beerFridge/update', beerFridgeController.updateFridge);
 
 
 export default routes;
