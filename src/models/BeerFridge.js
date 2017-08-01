6515fd8470d959fb5bb3eb533d9f853df2a8bbce
daniel kafer <daniel.j.kafer@gmail.com>
2017-08-01 16:17:12 -0500

    added beer fridge model

---
+++ src/models/BeerFridge.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import Beer from './Beer';
+import User from './User'
+
+const { Schema } = mongoose;
+
+const beerFridgeSchema = new Schema({
+	beers: [{
+		type: Schema.Types.ObjectId, ref: Beer
+	}],
+	owner: {
+		name: {type: Schema.Types.ObjectId, ref: User}
+	}
+});
+
+const BeerFridge = mongoose.model('BeerFridge', beerFridgeSchema);
+
+export default BeerFridge;
