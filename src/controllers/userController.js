6b502a109d584d6943c3aad72b35b14cdba35c4a
daniel kafer <daniel.j.kafer@gmail.com>
2017-10-02 11:48:01 -0500

    fixed user search, added add beer and delete user functions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,14 +24,16 @@ userController.postUser = (req, res) => {
 		});
 };
 
+//Find User
 userController.getUser = (req, res) => {
-	const { username } = req.body;
+	const { userSearch } = req.params;
 
-	db.User.find({username: username})
+	db.User.find({username: userSearch })
+		.populate('fridge')				
 		.then((user) => {
 			res.status(200).json({
 				success: true,
-				data: user
+				data: {id: user[0]._id, username: user[0].username, fridge: user[0].fridge}
 			});
 		})
 		.catch((err) => {
@@ -42,9 +44,34 @@ userController.getUser = (req, res) => {
 
 };
 
+
 //Login
 userController.getLogin = (req, res) => {
 	res.status(200).json({success: true});
+};
+
+//Add Beer to User
+userController.addBeer = (req, res) => {
+	const { user, beers } = req.params;
+	db.User.findOneAndUpdate({username: user},{fridge: beers}, {new: true}, (err, updatedUser) => {
+		if(err) {
+			return res.status(500).json({data: err});
+		}
+		db.User.populate(updatedUser, {path: 'fridge', model: 'Beer'}, (err, user) => {
+			return res.status(200).json({success: true, data: user});
+		});
+	});
+};
+
+//Delete User
+userController.deleteUser = (req, res) => {
+	const { userRemoved } = req.params;
+	db.User.remove({username: userRemoved}, (err, result) => {
+		res.status(200).json({
+			success: true,
+			data: result
+		});
+	});
 };
 
 export default userController;
