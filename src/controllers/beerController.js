ff4fccd1f349a290c272edc386781e4973b34463
daniel kafer <daniel.j.kafer@gmail.com>
2017-10-02 11:47:34 -0500

    added add beer function

--- src/controllers/beerController.js
+++ src/controllers/beerController.js
@@ -1,7 +1,40 @@
 import BreweryKey from '../../keys';
 import unirest from 'unirest';
+import db from './../models/index';
 
 const beerController = {};
+
+
+//New Beer
+beerController.addBeer = (req, res) => {
+	const { owner, beer, rating } = req.body;
+
+	const newBeer = new db.Beer({
+		name: beer.name,
+		description: beer.description,
+		abv: beer.abv,
+		ibu: beer.ibu,
+		label: beer.labels,
+		style: beer.style,
+		rating: rating,
+		owner: owner
+	});
+
+	newBeer.save()
+		.then((data) =>{
+			res.status(200).json({
+				success: true,
+				data: data
+			});
+		})
+		.catch((err) =>{
+			res.status(500).json({
+				data: err
+			});
+		});
+
+
+};
 
 beerController.getBeers = (req, res) => {
 	const { name, page } = req.params;
