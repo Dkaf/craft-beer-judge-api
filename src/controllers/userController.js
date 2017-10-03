ae788eca0fc7d914f32092ade6734402253d0471
daniel kafer <daniel.j.kafer@gmail.com>
2017-10-03 11:21:26 -0500

    added jwt for auth and bcrypt for password hash

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,31 @@
 import db from './../models/index';
+import app from '../app';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt-nodejs';
 const userController = {};
 
 // Create new user /signup route
 userController.postUser = (req, res) => {
 	const { username, password } = req.body;
 
+	const hash =  bcrypt.hashSync(password);
+
 	const user = new db.User({
 		username,
-		password
+		password: hash
 	});
 
 	user.save()
 		.then((newUser) => {
+			let payload = {
+				username: user.username,
+				id: user._id
+			};
+			let token = jwt.sign(payload, app.get('secret'), {expiresIn: '30m'});
 			res.status(200).json({
 				success: true,
-				data: newUser
+				data: newUser.username,
+				token: token
 			});
 		})
 		.catch((err) => {
@@ -46,41 +57,75 @@ userController.getUser = (req, res) => {
 
 
 //Login
-userController.getLogin = (req, res) => {
-	res.status(200).json({success: true});
+userController.login = (req, res) => {
+	const { name, password } = req.body;
+	db.User.findOne({'username': name})
+		.then((user) => {
+			if(!user) {
+				res.status(500).json({success: false, message:'User not found'});
+			} else if(user) {
+				if(!bcrypt.compareSync(password, user.password)) {									
+					res.status(500).json({success: false, message:'Login failed. Incorrect password'});
+				} else if(bcrypt.compareSync(password, user.password)) {
+					const payload = {
+						username: user.username,
+						id: user._id
+					};
+
+					let token = jwt.sign(payload, app.get('secret'), {
+						expiresIn: '24h'
+					});
+
+					res.status(200).json({success: true, token: token});
+					
+				}
+			}
+		}).catch((err) => {
+			throw err;
+		});
 };
 
 //Add Beer to User
 userController.addBeer = (req, res) => {
 	const { userId } = req.params;
 	const { beers } = req.body;
+
 	db.User.findOneAndUpdate({_id: userId}, {fridge: beers}, {new: true})
 		.populate('fridge')
 		.exec((err, user) => {
 			if(err) {
 				return res.status(500).json({success: false, data: err});				
+			} else if(user.username != req.decoded.username) {
+				return res.status(403).json({success: false, message:'Incorrect permissions'});
 			}
 			return res.status(200).json({success: true, data: user});			
 		});
 };
 
+
+
+
+
 //Delete User
 userController.deleteUser = (req, res) => {
 	const { userRemoved } = req.params;
-	db.User.remove({username: userRemoved})
-		.then((user) => {
-			res.status(200).json({
-				success: true,
-				data: user
+	if(userRemoved != req.decoded.username) {
+		return res.status(403).json({success: false, message:'Incorrect permissions'});
+	} else {
+		db.User.remove({username: userRemoved})
+			.then((user) => {
+				res.status(200).json({
+					success: true,
+					data: user
+				});
+			})
+			.catch((err) => {
+				res.status(500).json({
+					success: false,
+					data: err
+				});
 			});
-		})
-		.catch((err) => {
-			res.status(500).json({
-				success: false,
-				data: err
-			});
-		});
-
+	}
 };
 
 export default userController;
