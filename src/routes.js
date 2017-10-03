1346565ef70f18accc4a06bf479e5f156f0f56b2
daniel kafer <daniel.j.kafer@gmail.com>
2017-10-03 11:19:13 -0500

    added auth middleware and login

--- src/routes.js
+++ src/routes.js
@@ -5,23 +5,28 @@ const routes = express();
 //Controller Imports
 import userController from './controllers/userController';
 import beerController from './controllers/beerController';
-import beerFridgeController from './controllers/beerFridgeController';
+import authController from './controllers/authController';
 
 //User Routes
 routes.post('/signup', userController.postUser);
+routes.post('/login', userController.login);
 routes.get('/getuser/:userSearch', userController.getUser);
-routes.put('/user/addbeer/:userId', userController.addBeer);
-routes.delete('/deleteuser/:userRemoved', userController.deleteUser);
+routes.put('/user/addbeer/:userId', authController.verifyToken, userController.addBeer);
+routes.delete('/deleteuser/:userRemoved', authController.verifyToken, userController.deleteUser);
+
 //Beer Routes
-routes.post('/beer/addbeer', beerController.addBeer);
+routes.post('/beer/addbeer', authController.verifyToken, beerController.addBeer);
 routes.get('/beers/:name/:p', beerController.getBeers);
 routes.get('/categories', beerController.getCategories);
 routes.get('/categories/:categoryId', beerController.getSingleCategory);
 
+//Auth Middleware Testing Route
+// routes.get('/auth/test', authController.verifyToken);
+
 //Beer Fridge Routes
-routes.post('/beerfridge', beerFridgeController.createFridge);
-routes.get('/beerfridge/:user', beerFridgeController.getUserFridge);
-routes.put('/beerfridge/update', beerFridgeController.updateFridge);
+// routes.post('/beerfridge', beerFridgeController.createFridge);
+// routes.get('/beerfridge/:user', beerFridgeController.getUserFridge);
+// routes.put('/beerfridge/update', beerFridgeController.updateFridge);
 
 
 export default routes;
