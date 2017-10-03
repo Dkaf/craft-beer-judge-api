cd4931fa17504846c29f84eeec66fec9775b5bf6
daniel kafer <daniel.j.kafer@gmail.com>
2017-10-03 17:35:08 -0500

    changed authenticated user routes to use jwt user data instead of  req.params

--- src/routes.js
+++ src/routes.js
@@ -10,13 +10,14 @@ import authController from './controllers/authController';
 //User Routes
 routes.post('/signup', userController.postUser);
 routes.post('/login', userController.login);
-routes.get('/getuser/:userSearch', userController.getUser);
-routes.put('/user/addbeer/:userId', authController.verifyToken, userController.addBeer);
+routes.get('/getuser/', authController.verifyToken, userController.getUser);
+routes.put('/user/addbeer/', authController.verifyToken, userController.addBeer);
 routes.delete('/deleteuser/:userRemoved', authController.verifyToken, userController.deleteUser);
 
 //Beer Routes
 routes.post('/beer/addbeer', authController.verifyToken, beerController.addBeer);
-routes.get('/beers/:name/:p', beerController.getBeers);
+routes.delete('/beer', authController.verifyToken, beerController.removeBeer);
+routes.get('/beers/:name/:page', beerController.getBeers);
 routes.get('/categories', beerController.getCategories);
 routes.get('/categories/:categoryId', beerController.getSingleCategory);
 
